23516a6de818694d5def39acff625ff95ce07a99
Tilemaxos Alexiadis <talexiadis@lancom.gr>
2023-05-23 21:46:36 +0300

    new voiceChat flow

--- src/lib/socket/SocketInitEvents.ts
+++ src/lib/socket/SocketInitEvents.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import { EntityManager } from '@mikro-orm/core'
 import jwt from 'jsonwebtoken'
 import { Server } from 'socket.io'
@@ -9,19 +10,28 @@ import { FriendRequest } from '../../types/entities/FriendRequest'
 import { Group } from '../../types/entities/Group'
 import { GroupInvite } from '../../types/entities/GroupInvite'
 import { PersonalChat } from '../../types/entities/PersonalChat'
+import { PersonalMessage } from '../../types/entities/PersonalMessage'
 import { User } from '../../types/entities/User'
 import { VoiceChannel } from '../../types/entities/VoiceChannel'
-import { deleteMessageFromPersonalConversationAction, deleteTextChannelMessageAction, readMessageAction, sendMessageToFriendAction, sendMessageToTextChannelAction, SocketMessageRooms } from '../actions/MessageActions'
+import { MessageStateType } from '../../types/enums/MessageStateType'
+import {
+  deleteMessageFromPersonalConversationAction,
+  deleteTextChannelMessageAction,
+  readMessageAction,
+  sendMessageToFriendAction,
+  sendMessageToTextChannelAction,
+  SocketMessageRooms
+} from '../actions/MessageActions'
 
 export async function initSocketEvents (io: Server, em: EntityManager): Promise<void> {
   let disconnectAction: undefined | ReturnType<typeof setTimeout>
   io.on('connection', async (socket) => {
-    let user: User
+    let currentUser: User
     try {
-      user = jwt.verify(socket.handshake.auth.token as string, PRIVATE_KEY) as User
-      user = await em.findOneOrFail(User, user.id, { populate: ['groups', 'friendList', 'personalChats'] })
+      currentUser = jwt.verify(socket.handshake.auth.token as string, PRIVATE_KEY) as User
+      currentUser = await em.findOneOrFail(User, currentUser.id, { populate: ['groups', 'friendList', 'personalChats'] })
       disconnectAction && clearTimeout(disconnectAction)
-      user.isLoggedIn && em.assign(user, { isLoggedIn: true })
+      currentUser.isLoggedIn && em.assign(currentUser, { isLoggedIn: true })
     } catch (e) {
       console.log(e)
       socket.emit('authorization', 'failed')
@@ -30,30 +40,30 @@ export async function initSocketEvents (io: Server, em: EntityManager): Promise<
     }
 
     socket.emit('authorization', 'succeeded')
-    console.log(`User ${user.username} logged in`)
+    console.log(`User ${currentUser.username} logged in`)
 
-    em.assign(user, {
+    em.assign(currentUser, {
       socketId: socket.id
     })
 
     await em.flush()
-    await socket.join(`user:${user.id}`)
-    const groupsRooms = user.groups.getItems().map(group => `group:${group.id}`)
+    await socket.join(`user:${currentUser.id}`)
+    const groupsRooms = currentUser.groups.getItems().map(group => `group:${group.id}`)
     await socket.join(groupsRooms)
-    const personalChatRooms = user.personalChats.getItems().map(personalChat => `personal-chat:${personalChat.id}`)
+    const personalChatRooms = currentUser.personalChats.getItems().map(personalChat => `personal-chat:${personalChat.id}`)
     await socket.join(personalChatRooms)
 
     console.log('rooms', socket.rooms)
 
-    io.to([...user.friendList.getItems().map(fr => `user:${fr.id}`), ...groupsRooms]).emit('log-in')
+    io.to([...currentUser.friendList.getItems().map(fr => `user:${fr.id}`), ...groupsRooms]).emit('log-in')
 
     socket.on('message-send', async (data: SendMessageInputData) => {
       try {
         let socketMessageRooms: SocketMessageRooms
         if (data.personal) {
-          socketMessageRooms = await sendMessageToFriendAction(data, user, em)
+          socketMessageRooms = await sendMessageToFriendAction(data, currentUser, em)
         } else {
-          socketMessageRooms = await sendMessageToTextChannelAction(data, user, em)
+          socketMessageRooms = await sendMessageToTextChannelAction(data, currentUser, em)
         }
         io.to(socketMessageRooms.rooms).emit('message-receive', socketMessageRooms.message)
       } catch (e) {
@@ -63,7 +73,7 @@ export async function initSocketEvents (io: Server, em: EntityManager): Promise<
 
     socket.on('message-read', async (data: ReadMessageInputData) => {
       try {
-        const result = await readMessageAction(data, user, em)
+        const result = await readMessageAction(data, currentUser, em)
 
         io.to(result.rooms).emit('message-read', result.channel)
       } catch (e) {
@@ -75,9 +85,9 @@ export async function initSocketEvents (io: Server, em: EntityManager): Promise<
       try {
         let result: SocketMessageRooms
         if (data.personal) {
-          result = await deleteMessageFromPersonalConversationAction(data, user, em)
+          result = await deleteMessageFromPersonalConversationAction(data, currentUser, em)
         } else {
-          result = await deleteTextChannelMessageAction(data, user, em)
+          result = await deleteTextChannelMessageAction(data, currentUser, em)
         }
         emitMessageEvents(io, 'message-deleted', result)
       } catch (e) {
@@ -85,20 +95,78 @@ export async function initSocketEvents (io: Server, em: EntityManager): Promise<
       }
     })
 
-    socket.on('1st-send-voice-one-to-one', async (data: { toUserId: string, description: RTCSessionDescriptionInit }) => {
-      io.to(`user:${data.toUserId}`).emit('answer-call-one-to-one', { fromUserId: user.id, description: data.description })
+    socket.on('send-voice-one-to-one', async (data: { personalChatId: string, description: RTCSessionDescriptionInit }) => {
+      const personalChat = await em.findOne(PersonalChat, data.personalChatId, { populate: ['users'] })
+
+      if (!personalChat) {
+        socket.emit('answer-call-one-to-one', { err: 'User not found' })
+        return
+      }
+
+      const callMessage = em.create(PersonalMessage, {
+        from: currentUser,
+        personalChat,
+        isCall: true,
+        text: '',
+        state: MessageStateType.SENDED,
+        createdAt: new Date()
+      })
+
+      await em.persistAndFlush(callMessage)
+
+      io.to(`user:${personalChat.users.getItems().find(u => u.id !== currentUser.id)!.id}`).emit('receive-call-one-to-one', {
+        callMessage,
+        description: data.description
+      })
     })
 
-    socket.on('answered-one-to-one', async (data: { toUserId: string, description: RTCSessionDescriptionInit }) => {
-      io.to(`user:${data.toUserId}`).emit('call-accepted-one-to-one', { fromUserId: user.id, description: data.description })
+    socket.on('answer-call-one-to-one', async (data: { callMessageId: string, description?: RTCSessionDescriptionInit, answer: boolean }) => {
+      const callMessage = await em.findOne(PersonalMessage, data.callMessageId, { populate: ['personalChat.users'] })
+
+      if (!callMessage || callMessage.callData?.endTimestamp || callMessage.callData?.endTimestamp) {
+        io.to(`user:${currentUser.id}`).emit('answer-call-one-to-one', {
+          err: !callMessage ? 'This call message does not exist' : 'This call has ended'
+        })
+        return
+      }
+
+      em.assign(callMessage, {
+        callData: {
+          answer: data.answer
+        }
+      })
+
+      await em.flush()
+
+      const to = `user:${callMessage.personalChat.users.getItems().find(u => u.id !== currentUser.id)!.id}`
+      io.to(to).emit('answer-call-one-to-one', { callMessage, answer: data.answer, description: data.answer ? data.description : undefined })
     })
 
-    socket.on('send-candidate', async (data: { toUserId: string, candidate: RTCIceCandidate }) => {
-      io.to(`user:${data.toUserId}`).emit('receive-candidate', { fromUserId: user.id, candidate: data.candidate })
+    socket.on('send-candidate', async (data: { personalChatId: string, candidate: RTCIceCandidate }) => {
+      const personalChat = await em.findOne(PersonalChat, data.personalChatId, { populate: ['users'] })
+
+      if (!personalChat) {
+        io.to(`user:${currentUser.id}`).emit('receive-candidate', { err: 'This call message does not exit' })
+        return
+      }
+
+      const to = `user:${personalChat.users.getItems().find(u => u.id !== currentUser.id)!.id}`
+
+      io.to(to).emit('receive-candidate', { personalChat, candidate: data.candidate })
     })
 
-    socket.on('end-call-one-to-one', async (data: { toUserId: string }) => {
-      io.to(`user:${data.toUserId}`).emit('end-call-one-to-one')
+    socket.on('end-call-one-to-one', async (data: { callMessageId: string }) => {
+      const callMessage = await em.findOne(PersonalMessage, data.callMessageId)
+
+      if (!callMessage || callMessage.callData?.endTimestamp || callMessage.callData?.endCallingTimestamp) {
+        if (!callMessage || !callMessage?.isCall || callMessage.callData?.endCallingTimestamp || callMessage.callData?.endTimestamp) {
+          return
+        }
+      }
+
+      const to = `user:${callMessage.personalChat.users.getItems().find(u => u.id !== currentUser.id)!.id}`
+
+      io.to(to).emit('end-call-one-to-one')
     })
 
     socket.on('disconnect', async () => {
@@ -106,9 +174,9 @@ export async function initSocketEvents (io: Server, em: EntityManager): Promise<
         const secondsOfTimeout = SECONDS_FOR_LOGOUT
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         disconnectAction = setTimeout(async () => {
-          console.log(`User ${user.username} logged out`)
-          io.to([...user.friendList.getItems().map(fr => `user:${fr.id}`), ...groupsRooms]).emit('log-out', user)
-          em.assign(user, { isLoggedIn: false, lastLoggedInDate: new Date(new Date().valueOf() - secondsOfTimeout) })
+          console.log(`User ${currentUser.username} logged out`)
+          io.to([...currentUser.friendList.getItems().map(fr => `user:${fr.id}`), ...groupsRooms]).emit('log-out', currentUser)
+          em.assign(currentUser, { isLoggedIn: false, lastLoggedInDate: new Date(new Date().valueOf() - secondsOfTimeout) })
           await em.flush()
         }, secondsOfTimeout)
       } catch (e) {
--- src/types/entities/PersonalMessage.ts
+++ src/types/entities/PersonalMessage.ts
@@ -1,4 +1,5 @@
-import { Collection, Entity, ManyToOne, OneToMany } from '@mikro-orm/core'
+import { Collection, Embeddable, Embedded, Entity, ManyToOne, OneToMany, Property } from '@mikro-orm/core'
+import { GraphQLJSONObject } from 'graphql-type-json'
 import { Field, ObjectType } from 'type-graphql'
 import { PersonalChatUserPivot } from './LastReadMessagePivot'
 import { Message } from './Message'
@@ -14,4 +15,31 @@ export class PersonalMessage extends Message {
   @OneToMany(() => PersonalChatUserPivot, pivot => pivot.lastReadMessage)
   @Field(() => PersonalChatUserPivot)
     readBy = new Collection<PersonalChatUserPivot>(this)
+
+  @Property({ default: false })
+  @Field()
+    isCall: boolean
+
+  @Embedded(() => CallMetadata, { nullable: true })
+  @Field(() => GraphQLJSONObject, { nullable: true })
+    callData?: CallMetadata
+}
+
+@Embeddable()
+export class CallMetadata {
+  @Property({ nullable: true })
+  @Field({ nullable: true })
+    answer?: boolean
+
+  @Property({ nullable: true })
+  @Field({ nullable: true })
+    startTimestamp?: Date
+
+  @Property({ nullable: true })
+  @Field({ nullable: true })
+    endTimestamp?: Date
+
+  @Property({ nullable: true })
+  @Field({ nullable: true })
+    endCallingTimestamp?: Date
 }
