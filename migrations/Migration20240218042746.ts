eda895e3db29b04cfee1a14332a5ce1b30082c95
Tilemaxos Alexiadis <talexiadis@lancom.gr>
2024-02-18 06:38:10 +0200

    fix

--- migrations/Migration20240218042746.ts
+++ migrations/Migration20240218042746.ts
@@ -3,13 +3,16 @@ import { Migration } from '@mikro-orm/migrations'
 import { Group } from '../src/types/entities/Group'
 import { TextChannel } from '../src/types/entities/TextChannel'
 
-export class Migration20240218042745 extends Migration {
+export class Migration20240218042746 extends Migration {
   async up (): Promise<void> {
     const em = (await MikroORM.init()).em.fork()
 
     const groups = await em.find(Group, {}, { populate: ['members', 'owner'] })
 
     for (const group of groups) {
+      if (!group.members.getItems().map(m => m.id).includes(group.owner.id)) {
+        group.members.add(group.owner)
+      }
       const textChannels = await em.find(TextChannel, {}, { populate: ['users'] })
       for (const textChannel of textChannels) {
         textChannel.users.removeAll()
